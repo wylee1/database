2e96bc01d410933e6ed3e383760562cf20b3adf3
wylee <ebmmqmf@naver.com>
2024-11-17 22:13:17 +0900

    2024-11-17

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 try{
 Class.forName("com.mysql.cj.jdbc.Driver");
 Connection con=DriverManager.getConnection(
-"jdbc:mysql://192.168.56.101:3308/madang",
+"jdbc:mysql://192.168.56.101:4567/madang",
 "wylee",
 "madiq8047!@");
 Statement stmt=con.createStatement();
